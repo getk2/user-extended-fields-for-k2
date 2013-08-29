a232bc9e5fe8397fb0dce3703cbaf0c54209430c
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-08-29 19:26:21 +0300

    Updated to version 3.0

--- userExtendedFields.php
+++ userExtendedFields.php
@@ -1,53 +1,53 @@
 <?php
 /**
- * @version		2.1
+ * @version		3.0
  * @package		User Extended Fields for K2 (K2 plugin)
- * @author    JoomlaWorks - http://www.joomlaworks.net
- * @copyright	Copyright (c) 2006 - 2012 JoomlaWorks Ltd. All rights reserved.
+ * @author    	JoomlaWorks - http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die ;
 
 // Load the K2 plugin API
-JLoader::register('K2Plugin', JPATH_ADMINISTRATOR.DS.'components'.DS.'com_k2'.DS.'lib'.DS.'k2plugin.php');
+JLoader::register('K2Plugin', JPATH_ADMINISTRATOR.'/components/com_k2/lib/k2plugin.php');
 
-class plgK2Userextendedfields extends K2Plugin {
+class plgK2Userextendedfields extends K2Plugin
+{
 
 	// Required global reference parameters
-	var $pluginName 							= 'userExtendedFields';
-	var $pluginNameHumanReadable 	= 'User Extended Fields for K2';
-	var	$plgCopyrightsStart				= "\n\n<!-- JoomlaWorks \"User Extended Fields for K2\" Plugin (v2.1) starts here -->\n";
-	var	$plgCopyrightsEnd					= "\n<!-- JoomlaWorks \"User Extended Fields for K2\" Plugin (v2.1) ends here -->\n\n";
+	var $pluginName = 'userExtendedFields';
+	var $pluginNameHumanReadable = 'User Extended Fields for K2';
+	var $plgCopyrightsStart = "\n\n<!-- JoomlaWorks \"User Extended Fields for K2\" Plugin (v2.1) starts here -->\n";
+	var $plgCopyrightsEnd = "\n<!-- JoomlaWorks \"User Extended Fields for K2\" Plugin (v2.1) ends here -->\n\n";
 
-	function plgK2Userextendedfields(&$subject, $params) {
+	function plgK2Userextendedfields(&$subject, $params)
+	{
 		// Load the plugin language file the proper way
 		JPlugin::loadLanguage('plg_k2_'.$this->pluginName, JPATH_ADMINISTRATOR);
 
 		parent::__construct($subject, $params);
 	}
 
-	function onK2UserDisplay(&$user, &$params, $limitstart){
+	function onK2UserDisplay(&$user, &$params, $limitstart)
+	{
 
 		// API
-		$mainframe = &JFactory::getApplication();
-		$document  = &JFactory::getDocument();
-
-
+		$mainframe = JFactory::getApplication();
+		$document = JFactory::getDocument();
 
 		// ----------------------------------- Get plugin parameters -----------------------------------
 
 		// Global plugin params
-		$plugin =& JPluginHelper::getPlugin('k2',$this->pluginName);
-		$pluginGlobalParams = new JParameter($plugin->params);
-		$contactDetails = $pluginGlobalParams->get('contactDetails',0);
-		$socialProfiles = $pluginGlobalParams->get('socialProfiles',1);
+		$contactDetails = $this->params->get('contactDetails', 0);
+		$socialProfiles = $this->params->get('socialProfiles', 1);
 
 		// K2 User plugin specific params
 		$pluginParams = new K2Parameter($user->plugins, '', $this->pluginName);
 
-		if($contactDetails){
+		if ($contactDetails)
+		{
 			$address = $pluginParams->get('address');
 			$city = $pluginParams->get('city');
 			$stateOrProvince = $pluginParams->get('stateOrProvince');
@@ -56,7 +56,8 @@ class plgK2Userextendedfields extends K2Plugin {
 			$telephone = $pluginParams->get('telephone');
 			$mobile = $pluginParams->get('mobile');
 		}
-		if($socialProfiles){
+		if ($socialProfiles)
+		{
 			$facebook = $pluginParams->get('facebook');
 			$twitter = $pluginParams->get('twitter');
 			$google = $pluginParams->get('google');
@@ -68,32 +69,25 @@ class plgK2Userextendedfields extends K2Plugin {
 			$picasa = $pluginParams->get('picasa');
 		}
 
-
-
 		// ----------------------------------- Requirements -----------------------------------
-		require_once(dirname(__FILE__).DS.$this->pluginName.DS.'includes'.DS.'helper.php');
-
-
+		require_once (dirname(__FILE__).'/'.$this->pluginName.'/includes/helper.php');
 
 		// ----------------------------------- Head tag includes -----------------------------------
 		// Append head includes, but not when we're outputing raw content in K2
-		if(JRequest::getCmd('format')!='raw'){
-			$pluginCSS = userExtendedFieldsHelper::getTemplatePath($this->pluginName,'css/template.css');
+		if (JRequest::getCmd('format') != 'raw')
+		{
+			$pluginCSS = userExtendedFieldsHelper::getTemplatePath($this->pluginName, 'css/template.css');
 			$pluginCSS = $pluginCSS->http;
 			$document->addStyleSheet($pluginCSS);
 		}
 
-
-
 		// ----------------------------------- Fetch the template -----------------------------------
 		ob_start();
-		$getTemplatePath = userExtendedFieldsHelper::getTemplatePath($this->pluginName,'default.php');
+		$getTemplatePath = userExtendedFieldsHelper::getTemplatePath($this->pluginName, 'default.php');
 		$getTemplatePath = $getTemplatePath->file;
-		include($getTemplatePath);
+		include ($getTemplatePath);
 		$getTemplate = $this->plgCopyrightsStart.ob_get_contents().$this->plgCopyrightsEnd;
 		ob_end_clean();
-
-
 
 		// ----------------------------------- Output -----------------------------------
 		return $getTemplate;
--- userExtendedFields/includes/helper.php
+++ userExtendedFields/includes/helper.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * @version		2.1
+ * @version		3.0
  * @package		User Extended Fields for K2 (K2 plugin)
- * @author    JoomlaWorks - http://www.joomlaworks.net
- * @copyright	Copyright (c) 2006 - 2012 JoomlaWorks Ltd. All rights reserved.
+ * @author    	JoomlaWorks - http://www.joomlaworks.net
+ * @copyright	Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die ;
 
-class userExtendedFieldsHelper {
+class userExtendedFieldsHelper
+{
 
 	// Path overrides
-	public static function getTemplatePath($pluginName,$file){
-		$mainframe= JFactory::getApplication();
-		$p = new JObject;
-		if(file_exists(JPATH_SITE.DS.'templates'.DS.$mainframe->getTemplate().DS.'html'.DS.$pluginName.DS.str_replace('/',DS,$file))){
-			$p->file = JPATH_SITE.DS.'templates'.DS.$mainframe->getTemplate().DS.'html'.DS.$pluginName.DS.$file;
-			$p->http = JURI::root(true)."/templates/".$mainframe->getTemplate()."/html/{$pluginName}/{$file}";
-		} else {
-			if(K2_JVERSION == '15') {
-				$p->file = JPATH_SITE.DS.'plugins'.DS.'k2'.DS.$pluginName.DS.'tmpl'.DS.$file;
-				$p->http = JURI::root(true)."/plugins/k2/{$pluginName}/tmpl/{$file}";
-			} else {
-				$p->file = JPATH_SITE.DS.'plugins'.DS.'k2'.DS.$pluginName.DS.$pluginName.DS.'tmpl'.DS.$file;
-				$p->http = JURI::root(true)."/plugins/k2/{$pluginName}/{$pluginName}/tmpl/{$file}";
-			}
+	public static function getTemplatePath($pluginName, $file)
+	{
+		$mainframe = JFactory::getApplication();
+		$p = new stdClass;
+		if (file_exists(JPATH_SITE.'/templates/'.DS.$mainframe->getTemplate().'/html/'.$pluginName.'/'.$file))
+		{
+			$p->file = JPATH_SITE.'/templates/'.$mainframe->getTemplate().'/html/'.$pluginName.'/'.$file;
+			$p->http = JURI::root(true).'/templates/'.$mainframe->getTemplate().'/html/'.$pluginName.'/'.$file;
+		}
+		else
+		{
+			$p->file = JPATH_SITE.'/plugins/k2/'.$pluginName.'/'.$pluginName.'/tmpl/'.$file;
+			$p->http = JURI::root(true).'/plugins/k2/'.$pluginName.'/'.$pluginName.'/tmpl/'.$file;
 		}
 		return $p;
 	}
