de43ad7fbbc6b2f5f8ffda9e839096036ff1404b
Fotis Evangelou <fevangelou@gmail.com>
2014-06-05 18:06:49 +0300

    Updated release date

--- userExtendedFields/includes/helper.php
+++ userExtendedFields/includes/helper.php
@@ -3,7 +3,7 @@
  * @version		3.0
  * @package		User Extended Fields for K2 (K2 plugin)
  * @author    	JoomlaWorks - http://www.joomlaworks.net
- * @copyright	Copyright (c) 2006 - 2013 JoomlaWorks Ltd. All rights reserved.
+ * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
  * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  */
 
